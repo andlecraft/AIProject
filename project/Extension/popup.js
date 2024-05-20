51f7f7ff7c65357ce93c60cbe5fd9ce73356e5d6
andlecraft <42287993+andlecraft@users.noreply.github.com>
2024-05-20 20:46:21 +0900

    dd
    dd

--- project/Extension/popup.js
+++ project/Extension/popup.js
@@ -8,7 +8,7 @@ document.getElementById('highlightButton').addEventListener('click', () => {
         chrome.tabs.sendMessage(tabs[0].id, { action: "highlightSelection" }, (response) => {
             if (chrome.runtime.lastError) {
                 console.error(chrome.runtime.lastError);
-                alert("Failed to send message: " + chrome.runtime.lastError.message); // 현제 진행 상황
+                alert("Failed to send message: " + chrome.runtime.lastError.message); // 현재 진행 상황
                 return;
             }
             if (response && response.status === "success") {
